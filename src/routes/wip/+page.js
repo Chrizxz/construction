12f537adbd920b03cc5e401302be6a8929b4f48b
ChrizxzFTW <chrizxzftw@gmail.com>
2025-07-03 06:41:29 -0400

    Additions and Updates
    - Added the Atnip logo to the metadata
    - Updated the metadata description
    - Added custom page rendering

---
+++ src/routes/wip/+page.js
@@ -0,0 +1,7 @@
+export const prerender = true;
+export const load = async () => {
+    return {
+        title: 'Work in Progress',
+        description: 'This page is under construction. Please check back later for updates.'
+    };
+};
