4057c982d5dd8873d1abfd71d12547770083ab4c
ChrizxzFTW <chrizxzftw@gmail.com>
2025-07-19 19:43:46 -0400

    Updates and Deletions
    - Updated the svelte config to adjust back to old host
    - Removed unecessary packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
-const dev = process.env.NODE_ENV === 'development';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// adapter-static generates a static site for deployment
 		// See https://svelte.dev/docs/adapter-static for more information
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html', // or a specific fallback page, e.g., '200.html'
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: dev ? '' : '/construction'
-		},
-		prerender: {
-			handleMissingId: 'warn', // or 'ignore' or 'error'
-			entries: ['*']
-		}
+		adapter: adapter()
 	}
 };
 
